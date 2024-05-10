156f787f61ecfaede6ab90a64c6af3533a4b90c5
Zhu Zhen <zhen.z.zhu@seagate.com>
2024-05-10 13:49:21 +0800

    chore: Update GitHub Actions workflow permissions and App.js version number

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Version 1:  Edit <code>src/App.js</code> and save to reload.
+          Version 122:  Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
