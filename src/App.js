3ae4baa66f10c9b76bb96045adfbe19bee3cea27
Zhu Zhen <zhen.z.zhu@seagate.com>
2024-05-10 15:56:11 +0800

    chore: Update App.js to include a new version look paragraph

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Version 122:  Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Version look:  Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
